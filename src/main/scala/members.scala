9ddf813d6e1a2efda7ab1dd758ad9bd3a59ae02b
softprops <d.tangren@gmail.com>
2012-10-31 17:40:59 -0400

    file per method

---
+++ src/main/scala/members.scala
@@ -0,0 +1,27 @@
+package meetup
+
+import dispatch._
+
+trait Member { self: Client =>
+  case class MemberBuilder(id: String,
+                           fieldextra: Option[Seq[String]] = None,
+                           omits: Option[Seq[String]] = None,
+                           onlys: Option[Seq[String]] = None)
+       extends Completion {
+    def fields(fs: Seq[String]) = copy(fieldextra = Some(fs))
+    def omit(fs: String*) = copy(omits = Some(fs))
+    def only(fs: String*) = copy(onlys = Some(fs))
+
+    private def pmap = Map.empty[String, String] ++ fieldextra.map(
+      "fields" -> _.mkString(",")
+    ) ++ omits.map(
+      "omit" -> _.mkString(",")
+    ) ++ onlys.map(
+      "only" -> _.mkString(",")
+    )
+
+    override def request[T](handler: Client.Handler[T]) =
+      self.apply((self.apiHost / "2" / "member" / id) <<? pmap)(handler)
+  }
+  def member(id: String) = MemberBuilder(id)
+}
