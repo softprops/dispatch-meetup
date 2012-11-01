2d2b8cdceec2884bb4e244641d704f38ec180a41
softprops <d.tangren@gmail.com>
2012-11-01 18:28:07 -0400

    unneeded import

--- src/main/scala/stream/comments.scala
+++ src/main/scala/stream/comments.scala
@@ -1,7 +1,6 @@
 package meetup.stream
 
 import dispatch._
-import net.liftweb.json.JValue
 
 trait EventComments { self: Client =>
   def eventComments = StreamBuilder(self, streamHost / "2" / "event_comments")
